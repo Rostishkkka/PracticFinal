e6d1b3c4cc7380273702903b52d2bcec6a5eaa30
derekovskyyy <Derekovskyyy@gmail.com>
2023-06-21 11:20:48 +0300

    KJSdf

---
+++ TaskLog/TranslatorForDataGridColumns.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskLog
+{
+    internal static class TranslatorForDataGridColumns
+    {
+        public static string Translate(string name)
+        {
+            switch (name) 
+            {
+                case "TaskId":
+                    {
+                        return "ID Задачи";
+                    }
+                case "CompOemName":
+                    {
+                        return "Название компонента";
+                    }
+                case "UserName":
+                    {
+                        return "Создатель";
+                    }
+                case "DateTime":
+                    {
+                        return "Дата создания";
+                    }
+                case "EventType":
+                    {
+                        return "Статус";
+                    }
+            }
+            return $"None";
+        }
+    }
+}
